c47c91e9bbe22d3495f2dbf6f486a97092131200
Rodrigo Schmitz <rodrigo.schmitz@tekoa.com.br>
2016-02-09 00:06:28 -0200

    controller de produtos

--- first-api-restful/app.js
+++ first-api-restful/app.js
@@ -1,9 +1,7 @@
 var app = require('./config/app_config');
 var db = require('./config/db_config');
+var Protudo = require('./models/Produto');
 
 app.get('/', function(req, res) {
-	var pessoa = {
-		nome: 'Rodrigo Schmitz'
-	}
-	res.json(pessoa);
+	res.end('Bem-vindo a API de produtos');
 });

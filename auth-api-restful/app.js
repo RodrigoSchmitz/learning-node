36ac8a2788a465e3973a582ba4ff2a81b6a3847d
Rodrigo Schmitz <rodrigo.schmitz@tekoa.com.br>
2016-02-09 02:20:45 -0200

    criando modelo, controller e rotas de usuario - fazendo auth - protegendo rotas

--- auth-api-restful/app.js
+++ auth-api-restful/app.js
@@ -1,11 +1,13 @@
 var app = require('./config/app_config');
 var db = require('./config/db_config');
+var usuarios = require('./routes/usuarioRouter');
 var produtos = require('./routes/produtoRouter');
 
+
 app.get('/', function(req, res) {
-	res.end('Bem-vindo a API de produtos');
+	res.end('Bem-vindo a API de autenticação');
 });
 
-//rotas de produtos
-
+//rotas
+app.use('/usuarios',usuarios);
 app.use('/produtos',produtos);

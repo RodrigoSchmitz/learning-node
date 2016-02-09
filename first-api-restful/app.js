79d043e91f71053c42576b6f64cb734c99506214
Rodrigo Schmitz <rodrigo.schmitz@tekoa.com.br>
2016-02-08 23:03:53 -0200

    Instalando dependencias - express e body-parser - criando primeira rota

---
+++ first-api-restful/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var port = '3000';
+
+var app = express();
+
+app.listen(port);
+
+app.get('/', function(req, res) {
+	var pessoa = {
+		nome: 'Rodrigo Schmitz'
+	}
+	res.json(pessoa);
+});
